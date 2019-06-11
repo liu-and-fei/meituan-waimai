7c5f660dd46b61a2764a41273324ee496e408769
huachao <1169257384@qq.com>
2019-06-11 19:01:01 +0800

    我的页面初步完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(vant)
 
 Vue.config.productionTip = false;
 
