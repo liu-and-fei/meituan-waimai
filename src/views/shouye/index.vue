32a7a97112633545b6cc38551bf88154f4e002e6
liufei <1272274017@qq.com>
2019-06-11 22:22:20 +0800

    showSort编写

--- src/views/shouye/index.vue
+++ src/views/shouye/index.vue
@@ -1,3 +1,23 @@
 <template>
+  <div id="home">
     <h1>我是首页</h1>
+
+    <!--  帅选栏-->
+    <colation></colation>
+  </div>
 </template>
+
+<script>
+import colation from '@/components/shouye/colation';
+
+export default {
+  name: 'home',
+  components: {
+    colation
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// 配置文件
+
+module.exports = {
+  devServer: {
+    // 正向代理ip
+    proxy: 'http://i.waimai.meituan.com'
+  }
+};
