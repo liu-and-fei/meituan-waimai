d5e4ae90f33df12de21714a654fddb300fa4630d
liufei <1272274017@qq.com>
2019-06-12 13:27:15 +0800

    编译问题修复ok

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,12 @@ module.exports = {
   // 选项
   devServer: {
     proxy: {
-      // key - 请求前缀
-      // value - 代理选项设置
-      // http://localhost:8080/meituan/api/find => http://i.waimai.meituan.com/api/find
-      
       '/meituan': {
         target: 'http://i.waimai.meituan.com/',
         pathRewrite: {
           '^/meituan': ''
         }
-      },
+      }
+    }
+  }
 };
-
